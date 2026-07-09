c736e25608435eb31a1396ad78593a7aae308a27
IseeWiener <andrejlarsonprietzel@gmail.com>
2026-07-09 15:00:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'gymtracker-v9';
+const CACHE = 'gymtracker-v10';
 const ASSETS = ['./index.html','./style.css','./app.js','./manifest.json'];
 
 self.addEventListener('install', e => {
